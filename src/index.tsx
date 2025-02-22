8b0c528da5ee6ec19244b913f205b6174bc95a5e
deanjstone <deanojstone@icloud.com>
2025-02-22 18:22:52 +1100

    feat: refactor index.tsx to improve routing and error handling for the root element

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,31 @@
-import { render } from "solid-js/web";
+/* @refresh reload */
+import "./index.css";
+
+import { render, Suspense } from "solid-js/web";
 import { Router } from "@solidjs/router";
 import { createClient } from "@supabase/supabase-js";
 import { SupabaseProvider } from "solid-supabase";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-import "./index.css";
+import { routes } from "./routes";
 import App from "./App";
 
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 const root = document.getElementById("root");
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
+  );
+}
 
 render(
   () => (
     <SupabaseProvider client={supabase}>
-      <Router>
-        <App />
-      </Router>
+      <Router root={(props) => <App>{props.children}</App>}>{routes}</Router>
     </SupabaseProvider>
   ),
-  root!
+  root
 );
