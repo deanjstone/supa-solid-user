e4a7f4164384de1aab6a57fcc7ad849c102db216
deanjstone <deanojstone@icloud.com>
2025-03-01 14:49:16 +1100

    ✨ utils: add clamp and toggleValue functions for utility enhancements

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function clamp(val: number, min: number, max: number) {
+  return val > max ? max : val < min ? min : val;
+}
+
+export function toggleValue<T>(array: T[], value: T): T[] {
+  return array.includes(value)
+    ? array.filter((item) => item !== value)
+    : [...array, value];
+}
