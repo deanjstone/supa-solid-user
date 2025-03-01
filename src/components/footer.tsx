30727077efe976291d35a15381c4d0e10c19d92b
deanjstone <deanojstone@icloud.com>
2025-03-01 17:03:33 +1100

    💄 components: update Footer component to use dynamic link target and improve layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,11 @@
 import type { JSX } from "solid-js";
 import { A } from "@solidjs/router";
 
-function Link(props: { href: string; children: JSX.Element }) {
+function Link(props: { href: string; target: string; children: JSX.Element }) {
   return (
     <A
       href={props.href}
-      target="_blank"
+      target={props.target}
       rel="norefferer"
       class="font-medium underline underline-offset-4">
       {props.children}
@@ -15,11 +15,21 @@ function Link(props: { href: string; children: JSX.Element }) {
 
 export default function Footer() {
   return (
-    <footer class="border-t border-border/40 py-6 px-8 dark:border-border">
-      <div class="container flex flex-col items-center justify-between gap-4 ">
-        <p class="text-center text-sm leading-loose text-muted-foreground md:text-left">
+    <footer class="container border-t border-border/40 py-6 px-8 dark:border-border">
+      <div class="container flex items-center justify-between gap-4 ">
+        <p class="text-center text-sm leading-loose text-muted-foreground flex-col">
           Built & designed by{" "}
-          <Link href="https://github.com/deanjstone/">deanjstone</Link>
+          <Link href="https://github.com/deanjstone/" target="_blank">
+            @deanjstone
+          </Link>
+          . Read about the project{" "}
+          <A
+            href="/about"
+            rel="norefferer"
+            class="font-medium underline underline-offset-4">
+            here
+          </A>
+          .
         </p>
       </div>
     </footer>
