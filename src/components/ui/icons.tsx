658bc3780a7eef1259a52ea0eac1ca62ee8e8f17
deanjstone <deanojstone@icloud.com>
2025-03-01 13:55:55 +1100

    ✨ icons: add logout and link icons for UI enhancements

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -767,3 +767,22 @@ export function IconX(props: IconProps) {
     </Icon>
   );
 }
+
+export function IconLogOut(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <path d="M9 21h-4a2 2 0 0 1-2-2v-14a2 2 0 0 1 2-2h4" />
+      <path d="M16 17l5-5-5-5" />
+      <path d="M21 12h-12" />
+    </Icon>
+  );
+}
+
+export function IconLink(props: IconProps) {
+  return (
+    <Icon {...props}>
+      <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
+      <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
+    </Icon>
+  );
+}
