148e13dadcf430c2ae18f509a2d4d6fb8efcd7f6
deanjstone <deanojstone@icloud.com>
2025-03-01 17:03:53 +1100

    💄 pages: refactor Dashboard layout and integrate SignOut button

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,50 +2,35 @@ import { createSignal, createEffect } from "solid-js";
 import { useNavigate, A } from "@solidjs/router";
 import { useSupabase } from "solid-supabase";
 
-import { Button } from "~/components/ui/button";
-import { IconLogOut } from "~/components/ui/icons";
+import { Button } from "@ui/button";
 
+import { SignOut } from "~/components/buttons";
 import Footer from "~/components/footer";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const supabase = useSupabase();
 
-  createEffect(async () => {}, []);
+  // createEffect(async () => {}, []);
 
   return (
-    <div class="container flex-col">
-      <div class="mt-10 flex items-center justify-center">
-        <div class="w-full">
-          <div class="flex items-center justify-between">
-            <h1 class="text-2xl">Dashboard</h1>
-            <Button as={A} href="/profile">
-              Profile
-            </Button>
-            <Button
-              type="button"
-              variant="outline"
-              class="w-full sm:w-auto"
-              onClick={() => navigate("/profile")}>
-              Profile
-            </Button>
+    <>
+      <div class="container flex-col mt-8">
+        <div class="flex items-center justify-center">
+          <h1 class="text-2xl w-full">Dashboard</h1>
+          <div class="w-full">
+            <div class="flex items-center justify-between">
+              <Button href="/profile" as={A} variant="outline">
+                Profile
+              </Button>
 
-            <Button
-              type="button"
-              variant="destructive"
-              class="w-full sm:w-auto"
-              onClick={() => {
-                supabase.auth.signOut();
-                navigate("/");
-              }}>
-              Sign Out
-              <IconLogOut />
-            </Button>
+              <SignOut />
+            </div>
           </div>
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
