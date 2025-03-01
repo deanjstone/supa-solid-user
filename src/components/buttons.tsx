8d897d0d60d9f03b391044412ec32133f6e2d7db
deanjstone <deanojstone@icloud.com>
2025-03-01 19:06:39 +1100

    ✨ components: implement sign-out functionality using Supabase

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,15 +1,17 @@
 import { A } from "@solidjs/router";
 import { Button } from "@ui/button";
 import { IconLogOut } from "@ui/icons";
+import { useSupabase } from "solid-supabase";
 
 export function SignOut() {
+  const supabase = useSupabase();
   return (
     <Button
       variant="destructive"
       as={A}
       href="/signed-out"
       onClick={() => {
-        // supabase.auth.signOut();
+        supabase.auth.signOut();
       }}>
       Sign Out
       <IconLogOut />
