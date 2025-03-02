86d51bd7eacf0c282ca0831264a95ebf75ea326a
deanjstone <deanojstone@icloud.com>
2025-03-02 16:49:51 +1100

    🔥 config: remove Vercel configuration file and update base path for production

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  // Remove the base path setting for Vercel deployment
-  base: "",
+  base: process.env.NODE_ENV === "production" ? "/supa-solid-user" : "",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
